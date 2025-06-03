28c0818213e803cf81a20d423818c33036da7a3d
Ting3l <henning@gehebajo.de>
2025-06-03 15:23:34 +0200

    Update de-DE.js - Fix stupid Mistake

--- lambda/locale/de-DE.js
+++ lambda/locale/de-DE.js
@@ -1,19 +1,19 @@
-// locale/de-DE.js
+// locale/de-de.js
 module.exports = {
-  SpeakWelcome = 'Wilkommen bei Audiobookshelf.',
-  SpeakPlaying = 'Spiele',
-  SpeakBy = 'von',
-  SpeakTryAgain = 'Versuch es nochmal.',
-  SpeakPlaybackBookHandlerNoBookTitle = 'Das habe ich nicht verstanden. Versuch es mit "Ließ mir Titel von Autor vor".',
-  SpeakPlaybackBookHandlerNoBookFoundP1 = 'Kein Buch mit dem Titel',
-  SpeakPlaybackBookHandlerNoBookFoundP2 = 'gefunden. Versuch es nochmal.',
-  SpeakPlayBookIntentHandlerNoBookTitle = 'Das habe ich nicht verstanden. Versuch es mit "Ließ mir Titel von Autor vor".',
-  SpeakPlayBookIntentHandlerNoBookFound = "Kein entsprechendes Buch gefunden:",
-  SpeakUnsupportedAudioIntentHandler = 'Entschuldige, das kann ich noch nicht.',
-  SpeakHelpIntentHandler = 'Du kannst sagen "Ließ mir etwas vor" - wie kann ich helfen?',
-  SpeakCancelAndStopIntentHandlerGoodbye = 'Bis zum nächsten mal!',
-  SpeakFallbackIntentHandler = 'Entschuldige, das weiß ich nicht. Versuch es nochmal.',
-  SpeakIntentReflectorHandlerP1 = 'Du hast gerade',
-  SpeakIntentReflectorHandlerP2 = 'ausgelöst',
-  SpeakErrorHandler = 'Entschuldige, ich hatte ein Problem dabei. Versuch es nochmal.'
+  SpeakWelcome: 'Wilkommen bei Audiobookshelf.',
+  SpeakPlaying: 'Spiele',
+  SpeakBy: 'von',
+  SpeakTryAgain: 'Versuch es nochmal.',
+  SpeakPlaybackBookHandlerNoBookTitle: 'Das habe ich nicht verstanden. Versuch es mit "Ließ mir Titel von Autor vor".',
+  SpeakPlaybackBookHandlerNoBookFoundP1: 'Kein Buch mit dem Titel',
+  SpeakPlaybackBookHandlerNoBookFoundP2: 'gefunden. Versuch es nochmal.',
+  SpeakPlayBookIntentHandlerNoBookTitle: 'Das habe ich nicht verstanden. Versuch es mit "Ließ mir Titel von Autor vor".',
+  SpeakPlayBookIntentHandlerNoBookFound: "Kein entsprechendes Buch gefunden:",
+  SpeakUnsupportedAudioIntentHandler: 'Entschuldige, das kann ich noch nicht.',
+  SpeakHelpIntentHandler: 'Du kannst sagen "Ließ mir etwas vor" - wie kann ich helfen?',
+  SpeakCancelAndStopIntentHandlerGoodbye: 'Bis zum nächsten mal!',
+  SpeakFallbackIntentHandler: 'Entschuldige, das weiß ich nicht. Versuch es nochmal.',
+  SpeakIntentReflectorHandlerP1: 'Du hast gerade',
+  SpeakIntentReflectorHandlerP2: 'ausgelöst',
+  SpeakErrorHandler: 'Entschuldige, ich hatte ein Problem dabei. Versuch es nochmal.'
 };
